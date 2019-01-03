aeff649d9b0501cb403acfc2f5f031289dc331ce
Michael Davidson <michael.davidson@student.curtin.edu.au>
2019-01-03 08:59:18 +0000

    new API-Test filUpdates to config and API-Test.php

--- API-Test.php
+++ API-Test.php
@@ -1,11 +1,75 @@
-<h1>API Test Page</h1>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
 
-<p>I would like to use this page to start building the logic for our site. </p>
+    <title>Aussie Tipping API Test Page</title>
 
-<p>There will be not real styling to start with as I am still just getting a handle on the information side of things</p>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- Custom styles for this site -->
+    <link href="/css/styles.css" rel="stylesheet" type="text/css">>
+  </head>
 
-<p>I will start building navbar items so you can start seeing responses as I build them. Think of it as our own Docs</p>
+  <body>
 
-<a href="index.php">Home</a>
+    <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+      <a class="navbar-brand" href="#">Aussie Tipping</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">User APIs <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Competition APIs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sport APIs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home Page</a>
+          </li>
+         </ul>
+        <button type="button" class="btn btn-primary">Register</button>
+        <button type="button" class="btn btn-secondary">Login</button>
+  
+    </div>
+    </nav>
+
+   <main>
+       
+    </br>
+    </br>
+    </br>
+   <h1>API Test Page</h1>
+
+    <p>I would like to use this page to start building the logic for our site. </p>
+
+    <p>There will be not real styling to start with as I am still just getting a handle on the information side of things</p>
+
+    <p>I will start building navbar items so you can start seeing responses as I build them. Think of it as our own Docs</p>
+
+    
+
+    
+    </main><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
+
 
 
--- config.php
+++ config.php
@@ -1,4 +1,29 @@
 <?php
-//This file will contain configuration data for the database
+/* CONFIG
+* contains MySQL connection info and various
+* configuration variables and settings
+*/
+
+require 'functions.php';
+
+// Error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// MySQL database settings
+$dbhost = "localhost";
+$dbuser = "michaeldavidsond";
+$dbpass = "";
+$dbname = "Aussie_Tipping";
+
+
+// MySQL connection
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// Test if connection succeeded
+if(mysqli_connect_errno()) {
+  die("Database connection failed: " . mysqli_connect_error() ." (" . mysqli_connect_errno() . ")"
+  );
+}
 
 ?>
